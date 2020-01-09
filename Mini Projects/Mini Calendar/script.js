14645461be9f145eab1fadfaef58b8049d63dd9b
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-09 10:53:06 +0100

    .

---
+++ Mini Projects/Mini Calendar/script.js
@@ -0,0 +1,22 @@
+
+let oggi = new Date();
+let year = oggi.getFullYear();
+
+let oggi2 = new Date()
+let month = oggi2.getMonth()
+
+let oggi3 = new Date()
+let day = oggi3.getDay()
+
+let date = new Date()
+
+console.log(date);
+
+
+console.log(month);
+console.log(year);
+console.log(day);
+
+
+
+// let firstDay = (new Date(year:String, month:index[0-11])).getDay();
