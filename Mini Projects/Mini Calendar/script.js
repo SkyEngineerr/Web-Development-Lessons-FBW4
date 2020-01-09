8b761c268d5ef3af035fe4667e80caa5edb54d8d
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-09 12:58:55 +0100

    .

--- Mini Projects/Mini Calendar/script.js
+++ Mini Projects/Mini Calendar/script.js
@@ -2,30 +2,43 @@ const year = new Date().getFullYear();
 const month = new Date().getMonth()
 const day = new Date().getDay()
 
-const weekdays = [
-  'Sun',
-  'Mon',
-  'Tue',
-  'Wed',
-  'Thu',
-  'Fri',
-  'Sat'
-];
+const weekdays = [1, 2, 3, 4, 5, 6, 7];
 
 const firstDay = new Date(year, 0, 1).getDay();
 console.log(weekdays[firstDay]);
-
 console.log(month);
 console.log(year);
 console.log(day);
 
-let table = document.getElementById("myTable")
-console.log(table.rows[3]);
+let tableD = document.querySelectorAll('td')
 
-// if (weekdays[firstDay] == "Wed") {
-//     table.cells.item(0).innerHTML = '1'
-// }
+console.log(tableD[0]);
+
+let getDaysInMonth = function (month, year) {
+    return new Date(year, month, 0).getDate()
+}
+
+console.log(getDaysInMonth(0,2020));
+
+let count = weekdays[firstDay] - 1
+for (let i = 1; i < getDaysInMonth(0,2020)+1; i++) {
+    count = count + 1
+    tableD[count].innerHTML = i;
+}
 
 
 
 // let firstDay = (new Date(year:String, month:index[0-11])).getDay();
+
+
+////Next and Back Buttons////
+let bigDate = document.getElementById("bigDate")
+bigDate.innerHTML = month + ' ' + year
+
+const next = document.getElementById("Next");
+
+
+next.addEventListener('click', event => {
+  
+    bigDate.innerHTML = (month + 1 )+ ' ' + year;
+  });
