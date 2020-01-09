b46a62d4d9c4893984a95f5ffb96217ac9fb9abd
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-09 15:25:23 +0100

    .

--- Mini Projects/Mini Calendar/script.js
+++ Mini Projects/Mini Calendar/script.js
@@ -1,36 +1,42 @@
 let tableD = document.querySelectorAll('td')
 
-// console.log(tableD[0]);
-
-let getDaysInMonth = function (month, year) {
-     return new Date(year, month, 0).getDate()
-}
-
-
-
-////Next and Back Buttons////
-
 const submitButton = document.getElementById("submitButton");
 const inputMonth = document.getElementById('inputMonth')
 const inputYear = document.getElementById('inputYear')
-console.log(inputMonth);
+
 
 const weekdays = [1, 2, 3, 4, 5, 6, 7];
+const days = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
 
 
 submitButton.addEventListener('click', event => {
     event.preventDefault()
-    let firstDay = new Date(inputYear.value, inputMonth.value, 1).getDay();
-    bigDate.innerHTML = inputMonth.value + ' ' + inputYear.value;
-    console.log(weekdays[firstDay]);
+    let date = 0;
+
+    let firstDay = new Date(inputYear.value, inputMonth.value, 1);
+
+    date = new Date(inputYear.value, (inputMonth.value+1), 1);
+    lastDay = new Date(date - 1);
+    
+    console.log(firstDay);
+    console.log(lastDay);
+    console.log(inputYear.value);
+    console.log(inputMonth.value);
+    
+    bigDate.innerHTML = days[inputMonth.value] + ' ' + inputYear.value;
+
+    
 
     let count = weekdays[firstDay] -2
-    console.log(count);
+   
 
-    for (let i = 1; i < getDaysInMonth(inputMonth.value, inputYear.value) + 1; i++) {
+    for (let j=0; j<35; j++) {
+        tableD[j].innerHTML = '';
+    }
+  
+
+    for (let i = 1; i < (lastDay + 1); i++) {
         count = count +1 
         tableD[count].innerHTML = i; 
     }
-
-    
 });
