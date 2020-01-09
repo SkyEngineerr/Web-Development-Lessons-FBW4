1b559bb5db2d3d224048ef7ac62c5c0acf4dffa0
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-09 13:48:15 +0100

    .

--- Mini Projects/Mini Calendar/script.js
+++ Mini Projects/Mini Calendar/script.js
@@ -1,44 +1,36 @@
-const year = new Date().getFullYear();
-const month = new Date().getMonth()
-const day = new Date().getDay()
-
-const weekdays = [1, 2, 3, 4, 5, 6, 7];
-
-const firstDay = new Date(year, 0, 1).getDay();
-console.log(weekdays[firstDay]);
-console.log(month);
-console.log(year);
-console.log(day);
-
 let tableD = document.querySelectorAll('td')
 
-console.log(tableD[0]);
+// console.log(tableD[0]);
 
 let getDaysInMonth = function (month, year) {
-    return new Date(year, month, 0).getDate()
+     return new Date(year, month, 0).getDate()
 }
 
-console.log(getDaysInMonth(0,2020));
-
-let count = weekdays[firstDay] - 1
-for (let i = 1; i < getDaysInMonth(0,2020)+1; i++) {
-    count = count + 1
-    tableD[count].innerHTML = i;
-}
-
-
-
-// let firstDay = (new Date(year:String, month:index[0-11])).getDay();
 
 
 ////Next and Back Buttons////
-let bigDate = document.getElementById("bigDate")
-bigDate.innerHTML = month + ' ' + year
 
-const next = document.getElementById("Next");
+const submitButton = document.getElementById("submitButton");
+const inputMonth = document.getElementById('inputMonth')
+const inputYear = document.getElementById('inputYear')
+console.log(inputMonth);
+
+const weekdays = [1, 2, 3, 4, 5, 6, 7];
 
 
-next.addEventListener('click', event => {
-  
-    bigDate.innerHTML = (month + 1 )+ ' ' + year;
-  });
+submitButton.addEventListener('click', event => {
+    event.preventDefault()
+    let firstDay = new Date(inputYear.value, inputMonth.value, 1).getDay();
+    bigDate.innerHTML = inputMonth.value + ' ' + inputYear.value;
+    console.log(weekdays[firstDay]);
+
+    let count = weekdays[firstDay] -2
+    console.log(count);
+
+    for (let i = 1; i < getDaysInMonth(inputMonth.value, inputYear.value) + 1; i++) {
+        count = count +1 
+        tableD[count].innerHTML = i; 
+    }
+
+    
+});
