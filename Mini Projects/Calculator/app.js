96d4280a543bfefaab051eb2708063b2882684db
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-14 13:23:22 +0100

    .

--- Mini Projects/Calculator/app.js
+++ Mini Projects/Calculator/app.js
@@ -1,5 +1,7 @@
 const button = document.getElementsByClassName("numbers")
 const input = document.getElementById("input")
+const operator = document.getElementsByClassName("operator")
+const equalSign = document.getElementById("equal-sign-operator")
 
 console.log(input.value);
 
@@ -10,6 +12,29 @@ for (item of button) {
 
 function keyOutPut (e) {
     console.log(input.value);
-    input.value = e.target.value
+    input.value = input.value + '' +e.target.value
 }
 
+
+for (item of operator) {
+    item.addEventListener('click', operatorFunction)
+}
+
+let plusOp;
+let firstCount;
+
+function operatorFunction (e) {
+    console.log(e.target.value);
+    plusOp = e.target.value;
+    firstCount = input.value
+    input.value = ""
+    console.log(firstCount);
+    console.log(plusOp);
+}
+
+equalSign.addEventListener('click', function equal(e){
+    if(plusOp == '+') {
+        
+        input.value = firstCount + input.value
+    }
+})
