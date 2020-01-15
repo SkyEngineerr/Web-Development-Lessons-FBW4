7029c39f5169471761cebed94ab20cc0dac750ea
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-15 10:36:18 +0100

    .

--- Mini Projects/Calculator/app.js
+++ Mini Projects/Calculator/app.js
@@ -3,68 +3,87 @@ const input = document.getElementById("input")
 const operator = document.getElementsByClassName("operator")
 const equalSign = document.getElementById("equal-sign-operator")
 
+let arr = []
+
 console.log(input.value);
 
 
 for (item of button) {
     item.addEventListener('click', keyOutPut)
 }
+
 for (item of operator) {
     item.addEventListener('click', keyOutPut)
 }
 
+
 function keyOutPut (e) {
     //console.log(input.value);
-    input.value = input.value + '' +e.target.value
-}
-
-
-for (item of operator) {
-    item.addEventListener('click', operatorFunction)
-}
-
-
-let arr = []
-let plusSign;
-let minusSign;
-let multipleSign;
-let divideSign;
-
-function operatorFunction (e) {
-    console.log(e.target.value);
-    if(arr.length < 3) {
-        arr.push(input.value)
-        arr.push(e.target.value)
-        console.log(e.target.value)
-        console.log(arr)
-    }
-    if (arr.length > 3) {
-        if(arr[1] == '+') {
-            console.log(arr[0], arr[2])
-            arr.unshift(arr[0] + arr[2])
-            arr.push(input.value)
+    
+    if (e.target.className == 'numbers') {
+        if(arr.length < 2) {
+            input.value = input.value + '' + e.target.value
+        }
+        else if (arr.length == 2) {
+            if(input.value[0] == '+'){
+                input.value.slice(0,1)
+            }
+            else {
+                input.value = input.value + '' + e.target.value
+            }
             
-            arr.splice(2)
         }
-        else if(arr[1] == '-') {
-            arr.unshift(arr[0] - arr[2])
-            arr.splice(1)
-        }
-        else if(arr[1] == '/') {
-            arr.unshift(arr[0] / arr[2])
-            arr.splice(1)
-        }
-        else if(arr[1] == '*') {
-            arr.unshift(arr[0] * arr[2])
-            arr.splice(1)
-        }
-        console.log(arr)
+        
     }
-    
-    input.value = ""
-    console.log(arr)
-    
+
+    else if(e.target.className == 'operator') {
+        if(arr.length < 3) {
+            arr.push(input.value)
+            arr.push(e.target.value)
+            input.value = e.target.value
+        }
+    }
+  console.log(arr);
+  
 }
+
+
+
+// function operatorFunction (e) {
+//     console.log(e.target.value);
+//     if(arr.length < 3) {
+//         arr.push(input.value)
+//         arr.push(e.target.value)
+        
+//         console.log(arr)
+//     }
+//     else if (arr.length >= 3) {
+        
+//             console.log(arr[0], arr[2])
+//             arr.unshift(arr[0] + arr[2])
+//             arr.splice(1)
+//             arr.push(e.target.value)
+            
+        
+//         // else if(arr[1] == '-') {
+//         //     arr.unshift(arr[0] - arr[2])
+//         //     arr.splice(1)
+//         // }
+//         // else if(arr[1] == '/') {
+//         //     arr.unshift(arr[0] / arr[2])
+//         //     arr.splice(1)
+//         // }
+//         // else if(arr[1] == '*') {
+//         //     arr.unshift(arr[0] * arr[2])
+//         //     arr.splice(1)
+//         // }
+//         // console.log(arr)
+//     }
+    
+    
+//     console.log(arr)
+    
+// }
 
 equalSign.addEventListener('click', function equal(e){
    
