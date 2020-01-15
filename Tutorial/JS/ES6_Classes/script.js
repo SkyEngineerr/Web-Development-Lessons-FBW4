1e592741b9eb4a239b9f8f2c8fef44d9ac3a6a06
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-15 14:00:03 +0100

    .

--- Tutorial/JS/ES6_Classes/script.js
+++ Tutorial/JS/ES6_Classes/script.js
@@ -20,14 +20,37 @@ let u1 = new User('Hercule', '1234')
 console.log(u1);
 
 class Person {
-    constructor(firstName, lastName) {
+    constructor(firstName, lastName, dob) {
         this.name = firstName
         this.lastname = lastName
+        this.birthdate = dob
     }
     greetings () {
         return `Hello Mr. ${this.name} ${this.lastname}`
     }
+
+    getsMarried (newLastName) {
+        this.lName = newLastName
+    }
 }
 
-let p1 = new Person ('Eren' , 'Yilmaz')
+let p1 = new Person ('Eren' , 'Yilmaz', "6-25-1989")
+p1.getsMarried('Mercedes')
+
 console.log(p1.greetings());
+console.log(p1);
+
+class Book {
+    constructor (btitle, bauthor, bprice) {
+        this.title = btitle
+        this.author = bauthor
+        this.price = bprice
+    }
+    changePrice (newPrice) {
+        this.price = newPrice
+    }
+}
+
+let book1 = new Book ('Harry Potter', 'JKR', '12$')
+book1.changePrice('22$')
+console.log(book1);
