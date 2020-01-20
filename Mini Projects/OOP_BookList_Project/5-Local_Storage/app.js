138e906b284781965c2626c4743771e6741aca99
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-20 12:31:01 +0100

    .

--- Mini Projects/OOP_BookList_Project/5-Local_Storage/app.js
+++ Mini Projects/OOP_BookList_Project/5-Local_Storage/app.js
@@ -1,6 +1,6 @@
 //Book Constructor
-class Book{
-    constructor (title, author, isbn) {
+class Book {
+    constructor(title, author, isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
@@ -8,7 +8,7 @@ class Book{
 }
 
 //UI Constructor
-class UI{
+class UI {
     //Add Book To List
     addBookToList(book) {
         const list = document.getElementById('book-list')
@@ -20,36 +20,63 @@ class UI{
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href='#' class='delete'>X</a></td>`
-      list.appendChild(row)
+        list.appendChild(row)
     }
     // Clear fields
-    clearFields () {
+    clearFields() {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
     }
     // delete book
-    deleteBook (target) {
-        if(target.className === 'delete'){
+    deleteBook(target) {
+        if (target.className === 'delete') {
             target.parentElement.parentElement.remove();
-         }
+        }
     }
     ///Give Validation Feedback
-    showAlert (message, className) {
+    showAlert(message, className) {
         const div = document.createElement('div')
         div.className = `alert ${className}`
         div.appendChild(document.createTextNode(message))
         const container = document.querySelector('.container')
         const form = document.querySelector("#book-form")
         container.insertBefore(div, form)
-        setTimeout(function(){document.querySelector('.alert').remove()}, 3000);
+        setTimeout(function () {
+            document.querySelector('.alert').remove()
+        }, 3000);
     }
+}
+
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+          books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static displayBooks() {
+      
+    }
+    static addBook(book) {
+        const ui = new UI();
+        ui.addBookToList(book)
+    }
+    static removeBook(isbn) {
+
+    }
+
 }
 
 
 //Event Listeners
+//DOM Load Event
+document.addEventListener(`DOMContentLoaded`, Store.displayBooks)
 ////Event Listener for add book
-document.getElementById('book-form').addEventListener('submit', function(e){
+document.getElementById('book-form').addEventListener('submit', function (e) {
     //Get inputs values
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
@@ -60,23 +87,23 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     //Instantiate UI
     const ui = new UI();
     //Validation
-    if(title === '' || author === '' || isbn === ''){
+    if (title === '' || author === '' || isbn === '') {
         //Show Error feed Back
         ui.showAlert('Please fill in all the fields', 'error')
     } else {
-    //Add book to list
-    ui.addBookToList(book)
-    //Clear fields
-    ui.clearFields();
-    //Show Success feed Back
-    ui.showAlert('The Book Added Successfully', 'success')
-}
+        //Add book to list
+        ui.addBookToList(book)
+        //Clear fields
+        ui.clearFields();
+        //Show Success feed Back
+        ui.showAlert('The Book Added Successfully', 'success')
+    }
 
-    e.preventDefault(); 
+    e.preventDefault();
 })
 
 //// Event Listener for delete
-document.getElementById('book-list').addEventListener('click', function(e){
+document.getElementById('book-list').addEventListener('click', function (e) {
     //Int UI
     const ui = new UI();
 
@@ -85,15 +112,3 @@ document.getElementById('book-list').addEventListener('click', function(e){
     ui.showAlert('The Book Removed Successfully', 'success')
     e.preventDefault()
 })
-
-
-//Static Methods
-class User {
-    static method () {
-        return `Hi`
-    }
-}
-
-
-let user = new User
-console.log(user);
