9e38d1423414a2e4670a68b9d088f711589e2909
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-22 11:15:13 +0100

    .

--- Tutorial/JS/Fetch/ajax exercise/app.js
+++ Tutorial/JS/Fetch/ajax exercise/app.js
@@ -20,37 +20,48 @@ window.onload = function (){
     console.log("Document was loaded");
 }
 
+var div = document.createElement("div")
+
 function showUsers() {
     let xhr = new XMLHttpRequest()
     //console.log(Object.keys(xhr));
     xhr.open("GET", "users.json", true);
     xhr.onprogress = function() {
-        let p = document.createElement("p")
-        p.id = "status"
-        p.innerHTML = "SUPERERMAN"
-        document.body.appendChild(p)
+        
+        //p.id = "status"
+        div.innerHTML = "SUPERERMAN"
+        document.body.appendChild(div)
     }
+
     xhr.onreadystatechange = function () {
         
         switch (this.readyState) {
-            case 0:
-            p.innerHTML = "Request not initilazed"
-            break;
+            
             case 1:
-            p.innerHTML = "server connection established"  
-            break;
+                div.innerHTML = "server connection established"  
+                break;
             case 2:
-            p.innerHTML = "request received"  
-            break;
+                div.innerHTML = "request received"  
+                break;
             case 3:
-            p.innerHTML = "processing request" 
-            break; 
+                div.innerHTML = "processing request" 
+                break; 
             case 4:
-            p.innerHTML = "request finished and response is ready" 
-            break; 
+                let response = JSON.parse(this.responseText)
+                div.innerHTML = ""
+                response.forEach (function (item){
+                    let p = document.createElement("p")
+                    p.innerHTML = `Name: ${item.name} Phone: ${item.phone} Email: ${item.email}`
+                    div.appendChild(p)
+                })
+                console.log(response); 
+                break; 
+            default:
+                p.innerHTML = "Request not initilazed"
+                break;`1`
         }
     }
-    
+
     xhr.send()
 }
 
