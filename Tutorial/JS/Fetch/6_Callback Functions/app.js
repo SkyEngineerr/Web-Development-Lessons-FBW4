70b09f1386504cb59d858686feb0936c214c8bb0
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-27 12:30:05 +0100

    .

--- Tutorial/JS/Fetch/6_Callback Functions/app.js
+++ Tutorial/JS/Fetch/6_Callback Functions/app.js
@@ -9,8 +9,51 @@ const posts = [{
 
 
 //Syncronized Callback Function
-function createPost (post) {
+// function createPost (post) {
+//     setTimeout(function(){
+//         posts.push(post);
+//     },1000)
+// }
+
+
+// function getPosts(){
+//     setTimeout(function(){
+//         let output = ''
+//         posts.forEach(function(item){
+//             output += `<li>${item.body}</li>`
+//         })
+//         document.body.innerHTML = output
+//     }, 3000)
+// }
+
+// let p1 = {title: "Post three",
+// body: 'This is amazing post three'}
+
+// createPost(p1)
+// getPosts();
+
+
+//Callback Functipn
+function createPost (post, callback) {
     setTimeout(function(){
         posts.push(post);
-    }, 2000)
+        callback()
+    },1000)
 }
+
+
+function getPosts(){
+    setTimeout(function(){
+        let output = ''
+        posts.forEach(function(item){
+            output += `<li>${item.body}</li>`
+        })
+        document.body.innerHTML = output
+    }, 3000)
+}
+
+let p1 = {title: "Post three",
+body: 'This is amazing post three'}
+
+createPost(p1)
+getPosts();
