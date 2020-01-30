5536364d120fb503f1679b4f0189cbfb3371570c
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-30 11:40:30 +0100

    .

--- Tutorial/JS/Fetch/3_FETCH/app.js
+++ Tutorial/JS/Fetch/3_FETCH/app.js
@@ -1,16 +1,57 @@
-document.getElementById('button1').addEventListener('click', getText)
+document.querySelector('#button1').addEventListener('click', getText)
 
 
 console.log(window.fetch);
 function getText() {
-    fetch('local.txxt')
-    .then(function(res){
-        return res.text()
+    fetch('local.txt').then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then((responseJson) => {
+        // Do something with the response
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+}
+
+//Get local JSON file data
+
+document.querySelector('#button2').addEventListener('click', getJSON)
+
+function getJSON(){
+    fetch("local.json").then(function(response){
+        return response.json()
+    }).then(function(data){
+        const output = document.getElementById('output')
+        data.forEach(element => {
+            const li = document.createElement('li')
+            li.innerHTML = `Title is ${element.title} and body is ${element.body}`
+            output.appendChild(li)
+        });
     })
-    .then(function(data){
-        console.log(data);
+}
+
+
+
+document.querySelector('#button3').addEventListener('click', getApi)
+
+
+function getApi(){
+    fetch("https://api.github.com/users").then(function(response){
+        return response.json()
+    }).then(function(users){
+        const output = document.getElementById('output')
+        users.forEach(element => {
+            const li = document.createElement('li')
+            li.innerHTML = `Username: ${element.login}`
+            output.appendChild(li)
+        });
     })
-    .catch(function(problem) {
-        console.log(problem);
+    .catch(function(err){
+        console.log(err);
     })
 }
