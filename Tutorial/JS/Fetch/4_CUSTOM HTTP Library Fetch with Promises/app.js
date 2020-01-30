536f6c348db2d02b0cab2a3e3450b0f41ccd3aad
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-30 14:42:12 +0100

    .

--- Tutorial/JS/Fetch/4_CUSTOM HTTP Library Fetch with Promises/app.js
+++ Tutorial/JS/Fetch/4_CUSTOM HTTP Library Fetch with Promises/app.js
@@ -1,7 +1,24 @@
 const http = new EasyHTTP
 
 //get users
-http.get('https://api.github.com/users')
+// http.get('https://api.github.com/users')
+// .then(val => console.log(val))
+// .catch(er => console.log(er))
+
+//Create Users
+const user = {
+    name : 'EREN',
+    username : 'bomberman',
+    email: 'e.1989@gmail.com'
+}
+
+//Post user
+// http.post('https://jsonplaceholder.typicode.com/users', user)
+// .then(val => console.log(val))
+// .catch(er => console.log(er))
+
+//Update single User on server
+http.put('https://jsonplaceholder.typicode.com/users/1', user)
 .then(val => console.log(val))
 .catch(er => console.log(er))
 
--- Tutorial/JS/Fetch/4_CUSTOM HTTP Library Fetch with Promises/easyhttp2.js
+++ Tutorial/JS/Fetch/4_CUSTOM HTTP Library Fetch with Promises/easyhttp2.js
@@ -18,5 +18,36 @@
          })
      }
 
+    //Make a HTTP POST Request
+    post(url, data) {
+        return new Promise((resolve,reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
+
+    //Make a HTTP PUT Request
+    put(url,data) {
+        return new Promise ((resolve,reject) => {
+            fetch(url, {
+                method:'PUT',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
  }
 
