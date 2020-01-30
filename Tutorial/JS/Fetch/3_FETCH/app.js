e182c6fcb640b3dce23fa3692067925c1c70d7d3
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-01-30 09:54:04 +0100

    .

--- Tutorial/JS/Fetch/3_FETCH/app.js
+++ Tutorial/JS/Fetch/3_FETCH/app.js
@@ -1,7 +1,11 @@
 document.getElementById('button1').addEventListener('click', getText)
 
 
-console.log(window);
+console.log(window.fetch);
 function getText() {
-
+    fetch('local.txt')
+    .then(function(res){
+        console.log(res.text());
+    })
+    .catch()
 }
