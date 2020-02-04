1ea6ef5c9915d1c0a3ab129dad83b3ba319195e8
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-04 10:09:06 +0100

    .

--- Tutorial/JS/BROWSER/8_New Features JS2018 Iterators and Generators/2_Dating App Mini Project/app.js
+++ Tutorial/JS/BROWSER/8_New Features JS2018 Iterators and Generators/2_Dating App Mini Project/app.js
@@ -26,6 +26,26 @@ const data = [
 ]
 
 
+const profiles = profileIterator(data)
+//Call the first profile
+
+//Next Event
+document.getElementById('next').addEventListener('click', nextProfile)
+
+//Event Handler : display the next profile
+function nextProfile () {
+    const currentProfile =  profiles.next().value
+    document.getElementById('profileDisplay').innerHTML = `
+    <ul class='list-group>
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">age: ${currentProfile.age}</li>
+        <li class="list-group-item">gender: ${currentProfile.gender}</li>
+        <li class="list-group-item">lookingfor: ${currentProfile.lookingfor}</li>
+        <li class="list-group-item">location: ${currentProfile.location}</li>
+    </ul>'`
+    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}"`
+}
+
 //Profile Iterator
 function profileIter (profiles) {
     let nextIndex = 0;
@@ -33,7 +53,8 @@ function profileIter (profiles) {
     return {
         next : function(){
             return nextIndex < profiles.length?
-            {value : profiles[nextIndex++], done : false}
+            {value : profiles[nextIndex++], done : false}:
+            {done : true}
         }
     }
 }
