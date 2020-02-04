077ca8243ded28fec575ac24b76de5b6eefb0455
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-04 14:38:57 +0100

    .

---
+++ Tutorial/JS/BROWSER/8_New Features JS2018 Iterators and Generators/4_Maps/app.js
@@ -0,0 +1,11 @@
+//Object Extra Infos
+let person = {
+
+}
+
+person.age = 48;
+person['firstname'] = 'eren'
+let lastname = 'lastname'
+let value = 'yilmaz'
+person[lastname] = value;
+console.log(person);
