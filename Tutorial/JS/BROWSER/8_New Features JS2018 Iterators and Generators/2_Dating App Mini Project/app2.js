eee13637fac9ecf1ff3d2bb018c065ef6e085221
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-04 11:16:55 +0100

    .

--- Tutorial/JS/BROWSER/8_New Features JS2018 Iterators and Generators/2_Dating App Mini Project/app2.js
+++ Tutorial/JS/BROWSER/8_New Features JS2018 Iterators and Generators/2_Dating App Mini Project/app2.js
@@ -1,8 +1,9 @@
-
 const http = new EasyHTTP
 let counter = 1;
 let proxy = 'https://cors-anywhere.herokuapp.com/'
 http.get(proxy+'https://api.github.com/users/'+counter)
 .then(function(data){
-
+    if(response.status === 200) {
+        console.log(data);
+    }
 })
