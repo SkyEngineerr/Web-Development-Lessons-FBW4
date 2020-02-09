a4c36a7761dff539e94182410b5e861b15bc9084
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-09 23:06:02 +0100

    .

--- Mini Projects/Meal Finder/app.js
+++ Mini Projects/Meal Finder/app.js
@@ -1,3 +1,5 @@
+const http = new EasyHTTP
+
 const search = document.getElementById('search'),
   submit = document.getElementById('submit'),
   random = document.getElementById('random'),
@@ -5,15 +7,17 @@ const search = document.getElementById('search'),
   resultHeading = document.getElementById('result-heading'),
   comicSection = document.getElementById('comicSection');
 console.log(heroes);
+
+
 //Keys for MARVEL API
 const PRIV_KEY = "8bd6702a32eefc2c9cd4f8bf8b8e4f40e985c33b";
 const PUBLIC_KEY = "8b4cb10529415427d58dd88366be9a56";
 const ts = new Date().getTime();
 const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 
-const http = new EasyHTTP
 
-// Search hero and fetch from API
+
+// Searching the heroes or a hero and fetch from API
 function searchHero(e) {
   e.preventDefault();
 
@@ -32,15 +36,16 @@ function searchHero(e) {
         if (val.data.results === null) {
           resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
         } else if (term == 'deadpool') {
-          heroes.innerHTML = `<img src="deadpool/dp1.png" alt="" style="width:55rem;">`
+          heroes.innerHTML = `<img src="deadpool/dp1.png" class="img-fluid" alt="" style="width:55rem;">`
         } else{
-          console.log(val.data.results)
+          console.log(val.data.results);
+          
           heroes.innerHTML = val.data.results
             .map(
               hero => `
-            <div class="card-fluid col-lg-3 col-md-3 col-6" style="width: 12rem;">
+            <div class="card-fluid col-lg-3 col-md-3 col-6" data-heroID="${hero.id}" style="width: 12rem;">
                 <img class="card-img-top deneme" src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" alt="" style="height:10rem; width:10rem;">
-              <div class="card-body">
+              <div class="card-body hero-info">
                 <p class="card-text">${hero.name}</p>
               </div>
             </div>
@@ -55,6 +60,8 @@ function searchHero(e) {
   } else {
     alert('Please enter a search term');
   }
+
+ 
 }
 
 
@@ -129,11 +136,71 @@ function randomHero (e) {
         }
        
       })
-      
       .catch(er => console.log(er))
     // Clear search text
     search.value = '';
 } 
+
+//Get Hero By ID
+function getHeroById(heroID){
+  const heroid = heroID
+  http.get(`https://gateway.marvel.com/v1/public/characters/${heroid}?&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  .then(val => {
+    const hero = val.data.results[0]
+    heroes.innerHTML = `
+        <div class="card text-center" style="width: 100rem; ">
+                <div class="row no-gutters">
+                    <div class="col-auto">
+                        <img src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" class="img-fluid" alt="" style="width: 20rem;">
+                    </div>
+                    <div class="col text-left">
+                        <div class="card-block px-2">
+                            <h3 class="card-title mt-3">${hero.name}</h3>
+                            <p class="card-text">${hero.description}</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer w-100 text-muted text-right">
+                  Data provided by Marvel. © 2020 Marvel
+                </div>
+          </div>`
+  })
+  .catch(er => console.log(er))
+  http.get(`https://gateway.marvel.com/v1/public/characters/${heroid}/comics?format=comic&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+      .then(val => {
+        const comics = val.data.results
+        console.log(val.data)
+        for(item of comics) {
+          let charList = []
+          let creatorList = []
+          for (element of item.characters.items) {
+            charList.push(element.name)
+          }
+          for (element of item.creators.items) {
+            creatorList.push(element.name)
+          }
+          console.log(item)
+          comicSection.innerHTML += `
+          <div class="card-fluid col-lg-4 col-md-3 col-6 h-100 mt-3" id="comicCards">
+            <a href="#"><img src="${item.thumbnail.path+'.'+item.thumbnail.extension}" class="card-img-top" alt="" ></a>
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <p style="font-size: 12px;" class="card-text mb-0">${item.description}</p>
+            <p style="font-size: 12px;" class="card-text text-muted ">Characters: ${charList} </p>
+            <p style="font-size: 12px;" class="card-text text-muted">Creators: ${creatorList} </p>
+            <div class="card-footer">
+              <small style="line-height: 1;" class="text-muted"> Data provided by Marvel. © 2020 Marvel </small>
+            </div>
+          </div>`
+          charlist = []
+          creatorList = []
+        }
+       
+      })
+      .catch(er => console.log(er))
+}
 
 
 
@@ -144,58 +211,7 @@ random.addEventListener('click', randomHero);
 
 
 
-
-// $.getJSON(url, {
-//     ts: ts,
-//     apikey: PUBLIC_KEY,
-//     hash: hash
-//   })
-//   .done(function (data) {
-//     console.log(data.data);
-//     resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
-
-    // if (data.results === null) {
-    //   resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
-    // } else if (term == 'deadpool') {
-    //   heroes.innerHTML = `<img src="Photos/deadpool/dp3.png" alt="">`
-    // } else {
-//       console.log(data.data.results)
-//       const heroes = data.data.results
-//       const h1 = document.createElement('h1')
-//       h1.innerHTML ="saDSKLASMDKASMDLA"
-//       heroes.appendChild(h1)
-//       heroes.innerHTML = 'asdasdasdasdasddasdasdasd'
-//       console.log(heroes.innerHTML)
-//       // for(item of heroes) {
-//       //   heroes.innerHTML = 'asdasdasdasdasddasdasdasd'
-//       //   // heroes.innerHTML = `
-//       //   // <div class="col-lg-2 col-md-3 col-6 ">
-//       //   // <a href="#" class="d-block mb-4 h-100">
-//       //   //   <img class="img-fluid img-thumbnail" src="${item.thumbnail.path + '.' +item.thumbnail.extension}" alt="">
-//       //   // </a>
-//       //   // <div class="carousel-caption">
-//       //   //   <h4>${item.name}</h4>
-//       //   // </div>
-//       //   // </div>
-//       //   // `
-//       // }
-//       // heroes.innerHTML = data.results
-//       //   .map(
-//       //     hero => `
-//       //   <div class="col-lg-2 col-md-3 col-6 ">
-//       //   <a href="#" class="d-block mb-4 h-100">
-//       //     <img class="img-fluid img-thumbnail" src="${hero.image.url}" alt="">
-//       //   </a>
-//       //   <div class="carousel-caption">
-//       //     <h4>${hero.name}</h4>
-//       //   </div>
-//       // </div>
-//       // `
-//       // )
-//       // .join('');
-//     }
-//   })
-//   .fail(function(err){
-//     // the error codes are listed on the dev site
-//     console.log(err);
-//   });
+heroes.addEventListener('click',  e => {
+  const heroID = e.path[1].getAttribute('data-heroID')
+  getHeroById(heroID)
+})
