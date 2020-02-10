4187b0bbc782c1daed01be3a9688d8e59d2b0459
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-10 13:57:45 +0100

    .

--- Mini Projects/Meal Finder/app.js
+++ Mini Projects/Meal Finder/app.js
@@ -15,14 +15,13 @@ const PUBLIC_KEY = "8b4cb10529415427d58dd88366be9a56";
 const ts = new Date().getTime();
 const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 
-
-
 // Searching the heroes or a hero and fetch from API
 function searchHero(e) {
   e.preventDefault();
 
   // Clear single meal
   comicSection.innerHTML = '';
+  heroes.innerHTML = '';
 
   // Get search term
   const term = search.value;
@@ -33,25 +32,55 @@ function searchHero(e) {
     //get users
     http.get(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${term}&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
       .then(val => {
-        if (val.data.results === null) {
+        const heroesJS = val.data.results
+        let num = 1
+
+        if (heroesJS.length == 0) {
           resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
+          
         } else if (term == 'deadpool') {
           heroes.innerHTML = `<img src="deadpool/dp1.png" class="img-fluid" alt="" style="width:55rem;">`
         } else{
-          console.log(val.data.results);
-          
-          heroes.innerHTML = val.data.results
-            .map(
-              hero => `
-            <div class="card-fluid col-lg-3 col-md-3 col-6" data-heroID="${hero.id}" style="width: 12rem;">
-                <img class="card-img-top deneme" src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" alt="" style="height:10rem; width:10rem;">
-              <div class="card-body hero-info">
-                <p class="card-text">${hero.name}</p>
+          for (hero of heroesJS) {
+
+            if (hero.description == "" && hero.thumbnail.path.includes("image")){ 
+              heroes.innerHTML += `
+              <div class="card m-2" style="width:18rem;" data-heroID="${hero.id}">
+                <img class="card-img-top" src="Unknown avatars/${num}.jpg"  style="height:17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${hero.name}</h5>
+                  <p class="card-text" style="font-size:1vh">There is no information about the hero. If you want to get some information about the hero, please search on Google or Wikipedia.</p>
+                  <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
               </div>
-            </div>
-          `
-          )
-          .join('');
+              `
+              num++
+            }
+            else if (hero.description == "") {   
+              heroes.innerHTML += `
+              <div class="card m-2" style="width:18rem;" data-heroID="${hero.id}">
+                <img class="card-img-top" src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}"  style="height:17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${hero.name}</h5>
+                  <p class="card-text" style="font-size:1vh">There is no information about the hero. If you want to get some information about the hero, please search on Google or Wikipedia.</p>
+                  <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+              `
+            }
+            else {
+              heroes.innerHTML += `
+              <div class="card m-2" style="width:18rem;" data-heroID="${hero.id}">
+                <img class="card-img-top" src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}"  style="height:17rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${hero.name}</h5>
+                  <p class="card-text" style="font-size:1vh">${hero.description}</p>
+                  <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+              `
+            }   
+          }     
         }
       })
       .catch(er => console.log(er))
@@ -60,11 +89,7 @@ function searchHero(e) {
   } else {
     alert('Please enter a search term');
   }
-
- 
 }
-
-
 
 //Random number function for random click button
 function getRandomInt(min, max) {
@@ -73,38 +98,62 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; 
 }
 
-
-
 //Get random hero from API
 function randomHero (e) {
-  const randomID = getRandomInt(1010671, 1010698)
+  let randomID = getRandomInt(1009146, 1011143)
+  console.log(randomID)
   comicSection.innerHTML = ''
-  http.get(`https://gateway.marvel.com/v1/public/characters/${randomID}?&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+  let fetchNow = function () {http.get(`https://gateway.marvel.com/v1/public/characters/${randomID}?&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
       .then(val => {
-        const hero = val.data.results[0]
-        console.log(hero)
-        heroes.innerHTML = `
-        <div class="card text-center" style="width: 100rem; ">
-                <div class="row no-gutters">
-                    <div class="col-auto">
-                        <img src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" class="img-fluid" alt="" style="width: 20rem;">
-                    </div>
-                    <div class="col text-left">
-                        <div class="card-block px-2">
-                            <h3 class="card-title mt-3">${hero.name}</h3>
-                            <p class="card-text">${hero.description}</p>
-                            
+        if (val.code == 404) {
+          randomID = getRandomInt(1009146, 1011143)
+          console.log(randomID)
+          fetchNow()
+        }
+        else {
+          const hero = val.data.results[0]
+          if (hero.description == "") {
+            heroes.innerHTML = `
+            <div class="card text-center" style="width: 100rem; ">
+                    <div class="row no-gutters">
+                        <div class="col-auto">
+                            <img src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" class="img-fluid" alt="" style="width: 20rem;">
+                        </div>
+                        <div class="col text-left">
+                            <div class="card-block px-2">
+                                <h3 class="card-title mt-3">${hero.name}</h3>
+                                <p class="card-text">There is no information about "${hero.name}" in our database. If you want to get some information about the hero, you can visit "https://wwww.marvel.com"</p>
+                            </div>
+                            <div class="card-footer w-100 text-muted text-right" style="bottom:0; position:absolute;">
+                                Data provided by Marvel. © 2020 Marvel
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-footer w-100 text-muted text-right">
-                  Data provided by Marvel. © 2020 Marvel
-                </div>
-            </div>`
-      })
-      .catch(er => console.log(er))
+              </div>`
+          }
+          else {
+            heroes.innerHTML = `
+            <div class="card text-center" style="width: 100rem; ">
+                    <div class="row no-gutters">
+                        <div class="col-auto">
+                            <img src="${hero.thumbnail.path + '.' + hero.thumbnail.extension}" class="img-fluid" alt="" style="width: 20rem;">
+                        </div>
+                        <div class="col text-left">
+                            <div class="card-block px-2">
+                                <h3 class="card-title mt-3">${hero.name}</h3>
+                                <p class="card-text">${hero.description}</p>
+                            </div>
+                            <div class="card-footer w-100 text-muted text-right" style="bottom:0; position:absolute;">
+                                Data provided by Marvel. © 2020 Marvel
+                            </div>
+                        </div>
+                    </div>
+              </div>`
+          }
+        }
 
-      http.get(`https://gateway.marvel.com/v1/public/characters/${randomID}/comics?format=comic&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+        //Print all comics bottom of the hero section
+        http.get(`https://gateway.marvel.com/v1/public/characters/${randomID}/comics?format=comic&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
       .then(val => {
         const comics = val.data.results
         console.log(val.data)
@@ -119,16 +168,16 @@ function randomHero (e) {
           }
           console.log(item)
           comicSection.innerHTML += `
-          <div class="card-fluid col-lg-4 col-md-3 col-6 h-100 mt-3" id="comicCards">
+          <div class="card-fluid col-lg-4 col-md-3 col-12 h-100 mt-3" id="comicCards">
             <a href="#"><img src="${item.thumbnail.path+'.'+item.thumbnail.extension}" class="card-img-top" alt="" ></a>
-            <div class="card-body">
+            <div class="card-body" style="height:30rem;">
               <h5 class="card-title">${item.title}</h5>
-            </div>
-            <p style="font-size: 12px;" class="card-text mb-0">${item.description}</p>
-            <p style="font-size: 12px;" class="card-text text-muted ">Characters: ${charList} </p>
-            <p style="font-size: 12px;" class="card-text text-muted">Creators: ${creatorList} </p>
-            <div class="card-footer">
-              <small style="line-height: 1;" class="text-muted"> Data provided by Marvel. © 2020 Marvel </small>
+              <p style="font-size: 12px;" class="card-text mb-0">${item.description}</p>
+              <p style="font-size: 12px;" class="card-text text-muted mt-1 ">Characters: ${charList} </p>
+              <p style="font-size: 12px;" class="card-text text-muted">Creators: ${creatorList} </p>
+              <div class="card-footer text-center" style="bottom:0; position:absolute;">
+                <small style="line-height: 1;" class="text-muted"> Data provided by Marvel. © 2020 Marvel </small>
+              </div>
             </div>
           </div>`
           charlist = []
@@ -137,12 +186,21 @@ function randomHero (e) {
        
       })
       .catch(er => console.log(er))
+
+      })
+      .catch(er => console.log(er))
+  }
+      fetchNow()
+
+      
     // Clear search text
     search.value = '';
 } 
 
 //Get Hero By ID
 function getHeroById(heroID){
+  heroes.innerHTML = '';
+  comicSection.innerHTML = '';
   const heroid = heroID
   http.get(`https://gateway.marvel.com/v1/public/characters/${heroid}?&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`)
   .then(val => {
@@ -157,7 +215,6 @@ function getHeroById(heroID){
                         <div class="card-block px-2">
                             <h3 class="card-title mt-3">${hero.name}</h3>
                             <p class="card-text">${hero.description}</p>
-                            
                         </div>
                     </div>
                 </div>
@@ -182,16 +239,16 @@ function getHeroById(heroID){
           }
           console.log(item)
           comicSection.innerHTML += `
-          <div class="card-fluid col-lg-4 col-md-3 col-6 h-100 mt-3" id="comicCards">
+          <div class="card-fluid col-lg-4 col-md-3 col-12 h-100 mt-3" id="comicCards">
             <a href="#"><img src="${item.thumbnail.path+'.'+item.thumbnail.extension}" class="card-img-top" alt="" ></a>
-            <div class="card-body">
+            <div class="card-body" style="height:30rem;">
               <h5 class="card-title">${item.title}</h5>
-            </div>
-            <p style="font-size: 12px;" class="card-text mb-0">${item.description}</p>
-            <p style="font-size: 12px;" class="card-text text-muted ">Characters: ${charList} </p>
-            <p style="font-size: 12px;" class="card-text text-muted">Creators: ${creatorList} </p>
-            <div class="card-footer">
-              <small style="line-height: 1;" class="text-muted"> Data provided by Marvel. © 2020 Marvel </small>
+              <p style="font-size: 12px;" class="card-text mb-0">${item.description}</p>
+              <p style="font-size: 12px;" class="card-text text-muted mt-1 ">Characters: ${charList} </p>
+              <p style="font-size: 12px;" class="card-text text-muted">Creators: ${creatorList} </p>
+              <div class="card-footer text-center" style="bottom:0; position:absolute;">
+                <small style="line-height: 1;" class="text-muted"> Data provided by Marvel. © 2020 Marvel </small>
+              </div>
             </div>
           </div>`
           charlist = []
