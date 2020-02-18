5a81b4ce1fe20cfa152d0f26f2230bfc6a6fb3de
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-18 12:05:17 +0100

    .

---
+++ Tutorial/jQuery/5-Effects Animation/app.js
@@ -0,0 +1,60 @@
+$(document).ready(()=>{
+    $('#btnFadeOut').click(()=>{
+        $('#box').fadeOut(3000, ()=>{
+            $('#btnFadeOut').text("It's Gone")
+        })
+    })
+    $('#btnFadeIn').click(() =>{
+        $('#box').fadeIn(2000)
+    })
+    $('#btnFadeTog').click(() =>{
+        $('#box').fadeToggle(1000)
+    })
+    $('#btnSlideUp').click(() =>{
+        $('#box').slideUp(1000)
+    })
+    $('#btnSlideDown').click(() =>{
+        $('#box').slideDown(1000)
+    })
+    $('#btnSlideTog').click(() =>{
+        $('#box').slideToggle(1000)
+    })
+    $('#btnStop').click(() =>{
+        $('#box').stop()
+    })
+
+    $('#moveRight').click(() =>{
+        $('#box2').animate({
+            left:200,
+            height:'300px',
+            width:'300px',
+            opacitiy:"0,5"
+        })
+    })
+    $('#moveRight').click(() =>{
+        $('#box2').animate({
+            left:200,
+            height:'300px',
+            width:'300px',
+            opacitiy:"0,5"
+        })
+    })
+
+    $('#moveAround').click(() =>{
+        let box = $('#box2')
+        box.animate({
+            left:300
+        })
+        box.animate({
+            top:300
+        })
+        box.animate({
+            left:0
+        })
+        box.animate({
+            left:0,
+            top:0
+        })
+    })
+
+})
