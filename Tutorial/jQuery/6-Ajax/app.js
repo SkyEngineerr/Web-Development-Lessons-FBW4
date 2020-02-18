d13b49a69b06ae8cec9e3638175b905decbafbbd
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-18 15:04:13 +0100

    .

--- Tutorial/jQuery/6-Ajax/app.js
+++ Tutorial/jQuery/6-Ajax/app.js
@@ -1,11 +1,20 @@
 $(document).ready(()=>{
-    $('#results').load('test.html',
-    (responseTxt, statusTxt, xhr) => {
-        if(statusTxt == 'success') {
-            alert('it went fine')
-            $('#results').html(responseTxt)
-        }else if(statusTxt == 'error'){
-            alert('Error' + xhr.statusTxt)
-        }
+    // $('#results').load('test.html',
+    // (responseTxt, statusTxt, xhr) => {
+    //     if(statusTxt == 'success') {
+    //         alert('it went fine')
+    //         $('#results').html(responseTxt)
+    //     }else if(statusTxt == 'error'){
+    //         alert('Error' + xhr.statusTxt)
+    //     }
+    // })
+    // $.get('test.html', (data) => {
+    //     $('#results').html(data)
+    // })
+
+    $.getJSON('users.json', (data)=>{
+        $.each(data,(i,user)=>{
+            $('ul#users').append(`<li>${user.firsName}</li>`)
+        })
     })
 })
