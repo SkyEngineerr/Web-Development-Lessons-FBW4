ef87b51d7a7026366323bc6e85213e4b68d890b9
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-19 12:24:27 +0100

    .

--- Tutorial/Modules/Functional components/main.js
+++ Tutorial/Modules/Functional components/main.js
@@ -1,12 +1,16 @@
 // import {greeting, hi} from './user.js';
-
 // console.log(hi('EREN'));
 
-
 // import {sqr} from './user.js';
-
 // console.log(sqr(2));
 
+// import sqr from "./user.js"
+// console.log(sqr(2));
 
-import square from "./user.js"
-console.log(square(17));
+// import {square, makeElement} from "./user.js"
+// makeElement('h3', square(), 'red')
+
+import {addPerson} from "./user.js"
+
+addPerson('EREN')
+addPerson('deneme')
--- Tutorial/Modules/Functional components/user.js
+++ Tutorial/Modules/Functional components/user.js
@@ -1,4 +1,5 @@
 //ADD EXPORT AFTER DECLARATION
+
 // let greeting = 'welcome back'
 
 // function hi (user) {
@@ -8,6 +9,35 @@
 // export {greeting, hi}
 
 //ADD EXPORT BEFORE DECLARATION
-export default function sqr (num) {
-    return num*num
+// function square (num) {
+//     return 'i am here for making problems'
+// }
+// export{square, makeElement}
+
+// export default function sqr (num) {
+//     return num*num
+// }
+
+// function makeElement (tagName, text, background) {
+//     let el = document.createElement(tagName)
+//     el.append(text);
+//     el.style.background = background;
+//     el.style.color = '#fff'
+//     el.style.height = '100px'
+//     document.body.append(el)
+// }
+
+export {addPerson}
+
+let ul = document.createElement('ul')
+document.body.appendChild(ul)
+let newArr = [];
+
+function addPerson (person) {
+    newArr.push(person)
+    let li = document.createElement('li')
+    li.innerHTML = person
+    ul.appendChild(li)
 }
+
+console.log(newArr);
