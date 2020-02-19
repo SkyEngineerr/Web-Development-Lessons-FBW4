db9b1e70854649e418b66a1ea8271c8cdce11bb4
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-19 09:12:40 +0100

    .

---
+++ Tutorial/jQuery/Slider Exercise/app.js
@@ -0,0 +1,29 @@
+// $(document).ready(()=>{
+//     let count = 2;
+
+//     $('#btnRight').click(()=>{
+//         if (count < 5) {
+//             $('#image').attr("src",`${count}.jpg`)
+//             count++
+            
+//         } else if (count == 5) {
+//             count = 1;
+//             $('#image').attr("src",`${count}.jpg`)
+//         }
+//     })
+
+    
+//     $('#btnLeft').click(()=>{
+//         console.log(count);
+//         if (count > 0) {
+//             console.log(count);
+//             $('#image').attr("src",`${count}.jpg`)
+//             count--
+//         } else if (count == 0) {
+//             count = 5;
+//             $('#image').attr("src",`${count}.jpg`)
+//         }
+//     })
+
+
+// })
