4d41da6bf8733d8f1231dc03e565b79148107c68
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-25 14:05:21 +0100

    .

---
+++ React/Deneme/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+//import logo from './logo.svg';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <header className='App'> Hello Fbw4 </header>;
+  }
+}
+
+export default App;
---
+++ React/Deneme/src/components/layout/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return <div className='navbar bg-primary'></div>;
+  }
+}
+
+export default Navbar;
