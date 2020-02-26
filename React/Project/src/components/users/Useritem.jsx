8712280ef27134144eb1b52b9aeb984d850aa117
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-02-26 13:51:36 +0100

    .

--- React/Project/src/components/users/Useritem.jsx
+++ React/Project/src/components/users/Useritem.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 class Useritem extends Component {
-  constructor() {
-    this.state = {
-      x: "me"
-    };
-  }
+  state = {
+    id: 1,
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo"
+  };
   render() {
-    return <div></div>;
+    return (
+      <div className='card text-center'>
+        <a href={this.state.html_url} target='_blank' rel='noopener noreferrer'>
+          <img
+            src={this.state.avatar_url}
+            alt=''
+            className='round-img'
+            style={{ width: "60px" }}
+          />
+        </a>
+
+        <h3>{this.state.login}</h3>
+      </div>
+    );
   }
 }
 
