a36e330f2e1a688025e3d7bc634164ce310e7fb4
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-03-09 11:06:37 +0100

    .

---
+++ React/Daily Exercise (Toggle)/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true
+    };
+  }
+  render() {
+    return (
+      <>
+        <div>deneme</div>
+      </>
+    );
+  }
+}
+
+export default App;
