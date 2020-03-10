1b9233e130ac255c0a6844eae1abcb40e6a6c454
Eren <48951264+SkyEngineerr@users.noreply.github.com>
2020-03-10 15:10:10 +0100

    .

--- React/usersearch3/src/App.jsx
+++ React/usersearch3/src/App.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar.jsx';
-
-import Users from './components/users/Users.jsx';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar.jsx";
+import axios from "axios";
+import Users from "./components/users/Users.jsx";
 class App extends Component {
+  state = {};
+  async componentDidMount() {
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+  }
   render() {
     //const title = 'User Search';
     //const number = [1, 2, 3, 4];
